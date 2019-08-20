c126bbbbc437b401d53fa593846eedc853810f1c
MIMIEYES <pierreluohao@gmail.com>
2019-08-21 00:43:27 +0800

    add @Required

--- src/main/java/io/nuls/token/NRC721Receiver.java
+++ src/main/java/io/nuls/token/NRC721Receiver.java
@@ -26,7 +26,8 @@ package io.nuls.token;
 import io.nuls.contract.sdk.Address;
 import io.nuls.contract.sdk.Contract;
 import io.nuls.contract.sdk.Msg;
-import io.nuls.token.interfaces.NRC721TokenReceiver;
+import io.nuls.token.base.NRC721ReceiverBase;
+import io.nuls.token.interfaces.INRC721TokenReceiver;
 import io.nuls.token.model.NRC721TransferRecord;
 
 import java.math.BigInteger;
@@ -39,52 +40,8 @@ import static io.nuls.contract.sdk.Utils.require;
  * @author: PierreLuo
  * @date: 2019-06-13
  */
-public class NRC721Receiver implements NRC721TokenReceiver, Contract {
+public class NRC721Receiver extends NRC721ReceiverBase implements Contract {
 
-    /**
-     * nrc721Address
-     * -- tokenId, NRC721TransferRecord
-     */
-    private Map<Address, Map<BigInteger, NRC721TransferRecord>> nrc721tokenMap = new HashMap<Address, Map<BigInteger, NRC721TransferRecord>>();
-
-    @Override
-    public boolean onNRC721Received(Address operator, Address from, BigInteger tokenId, String data) {
-        Address nrc721 = Msg.sender();
-        if (!nrc721.isContract()) {
-            return false;
-        }
-        Map<BigInteger, NRC721TransferRecord> map = nrc721tokenMap.get(nrc721);
-        if (map == null) {
-            map = new HashMap<BigInteger, NRC721TransferRecord>();
-            map.put(tokenId, new NRC721TransferRecord(operator, from));
-            nrc721tokenMap.put(nrc721, map);
-            return true;
-        }
-        NRC721TransferRecord nrc721TransferRecord = map.get(tokenId);
-        if (nrc721TransferRecord != null) {
-            return false;
-        }
-        nrc721TransferRecord = new NRC721TransferRecord(operator, from);
-        map.put(tokenId, nrc721TransferRecord);
-        return true;
-    }
-
-    public void transferOtherNRC721(Address nrc721, Address to, BigInteger tokenId) {
-        require(!Msg.address().equals(nrc721), "Do nothing by yourself");
-        require(nrc721.isContract(), "[" + nrc721.toString() + "] is not contract address");
-        Map<BigInteger, NRC721TransferRecord> map = nrc721tokenMap.get(nrc721);
-        require(map != null, "No NRC721 token received in [" + nrc721.toString() + "]");
-        require(map.remove(tokenId) != null, "No NRC721 token received in [" + nrc721.toString() + "]");
-
-        String methodName = "transferFrom";
-        if(to.isContract()) {
-            methodName = "safeTransferFrom";
-        }
-        String[][] args = new String[][]{
-                new String[]{Msg.address().toString()},
-                new String[]{to.toString()},
-                new String[]{tokenId.toString()}};
-        nrc721.call(methodName, "(Address from, Address to, BigInteger tokenId) return void", args, BigInteger.ZERO);
-
+    public NRC721Receiver() {
     }
 }
